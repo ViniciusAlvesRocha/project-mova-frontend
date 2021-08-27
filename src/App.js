7930f5af1a88c3cfdd42c66f9fd294029210671a
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-08-27 11:41:02 -0300

    acrescentando tag HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Pagination from './components/Pagination';
 import Home from "./pages/Home";
 import CountryDetails from './pages/CountryDetails';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import GlobalStyle from './styles/globals';
@@ -12,14 +12,14 @@ function App() {
   return (<>
     <GlobalStyle />
       <Provider store={ store }>
-        <BrowserRouter>
+        <HashRouter>
           <Header />
             <Switch>
               <Route path="/" exact={ true } component={ Home } />
               <Route path="/country/:code" component={ CountryDetails } />
             </Switch>
           <Pagination />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
   </>);
 }
